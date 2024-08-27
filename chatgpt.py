2d22024f46a4a933d004155c87e054ff58063082
devbelek <belekasrarov10@gmail.com>
2024-08-27 20:07:36 +0600

    Добавлено фото студента

--- chatgpt.py
+++ chatgpt.py
@@ -23,7 +23,7 @@ async def generate_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
         response = co.generate(
             model='command-xlarge-nightly',
             prompt=user_message,
-            max_tokens=300,
+            max_tokens=1500,
             temperature=0.7
         )
         generated_text = response.generations[0].text.strip()
