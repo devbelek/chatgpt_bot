0e593b8a9065eb1328c1ce431a2e2aa78fc7ecf8
devbelek <belekasrarov10@gmail.com>
2024-08-27 19:36:13 +0600

    sdfds

--- chatgpt.py
+++ chatgpt.py
@@ -1,9 +1,8 @@
-import asyncio
 import os
 import cohere
+import asyncio
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
-
 
 cohere_api_key = 'pdmEdFBDZqFYH9ljBReMM4VzSPIOwij2bJZNiPwv'
 co = cohere.Client(cohere_api_key)
@@ -11,10 +10,9 @@ co = cohere.Client(cohere_api_key)
 telegram_token = '7406870442:AAEcOkcBKC2FOP17s4BuY87UDmAA884NIfI'
 webhook_url = os.getenv('WEBHOOK_URL', 'https://chatgpt-bot-9wty.onrender.com')
 
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Отправляет сообщение при команде /start."""
     await update.message.reply_text('Привет! Я бот, использующий Cohere API. Напишите мне что-нибудь, и я отвечу!')
-
 
 async def generate_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_message = update.message.text
@@ -34,34 +32,23 @@ async def generate_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
         print(f"Произошла ошибка: {e}")
         await update.message.reply_text("Извините, произошла ошибка при обработке вашего запроса.")
 
-
 async def main():
     application = Application.builder().token(telegram_token).build()
 
-
+    # Настройка Webhook
     await application.bot.set_webhook(url=webhook_url)
-
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, generate_text))
 
-    application.run_webhook(
+    # Запуск Webhook
+    await application.run_webhook(
         listen="0.0.0.0",
-        port=int(os.environ.get("PORT", 8443)),
+        port=int(os.getenv("PORT", 8443)),
         url_path="",
         webhook_url=webhook_url
     )
 
-async def main():
-    application = Application.builder().token(telegram_token).build()
-
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, generate_text))
-
-    await application.initialize()
-    await application.start()
-    await application.updater.start_polling()
-    await application.updater.idle()
-
 if __name__ == '__main__':
     asyncio.run(main())
+
